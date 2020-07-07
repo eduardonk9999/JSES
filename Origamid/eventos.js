a44274ea8a564072e7f48b2b90acd559d0b0109b
Eduardo <thiago.novak7@gmail.com>
2020-07-07 15:25:10 -0300

    Add, aulas

---
+++ Origamid/eventos.js
@@ -0,0 +1,7 @@
+//Primeiro parâmetro do callback é referente ao evento que ocorreu.
+
+event.currentTarget; // this
+event.target; // Onde o clique ocorreu
+event.type; // tipo de evento
+
+ 
---
+++ Origamid/transversingemanipulacao.js
@@ -0,0 +1,7 @@
+/*Transversing*/
+
+lista.parentElement; //pai
+lista.parentElement.parentElement; // pai do pai
+lista.previousElementSibling; //elemento abaixo
+lista.children; // HTMLCollection com os filhos
+lista.children[0]; //primeiro filho
