9f108a787f492f2cb8e741747d51f43ba36858df
Eduardo <thiago.novak7@gmail.com>
2020-07-23 16:18:34 -0300

    Add, aula de construct

---
+++ Origamid/construcotFunctions.js
@@ -0,0 +1,13 @@
+function Carro() {
+    this.marca = 'Marca';
+    this.preco = 0;
+}
+
+const honda = new Carro();
+honda.marca = 'Honda';
+honda.preco = 4000;
+
+const fiat = new Carro();
+fiat.marca = 'Fiat';
+fiat.preco = 3000;
+
